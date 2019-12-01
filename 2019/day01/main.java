edf617c7c447f7eee471579395c08898e75c5687
teddy <teddyjh@protonmail.com>
2019-12-01 11:12:18 +0100

    init commit with loads of code
    First commit for the first day of Advent of Code!
    Time to practice some java, because it's fun.
    
    I'm trying to do a simple TDD approach where I write a test
    module/method for my other methods. Here we have already
    made a method to calculate the fuel needed for Santas spaceship
    and we are going to test it for the 4 given masses in the
    exercise/assignment.

---
+++ 2019/day01/main.java
@@ -0,0 +1,63 @@
+class main {
+
+   public static void main(String[] args){
+      System.out.println("Hello!"); 
+      Tester();
+
+   }
+
+   public static void Tester()
+   {
+       double[] masses = {12};
+       double[] fuelAnswers = {2};
+       double mass;
+       double fuelAnswer;
+       
+       for(int i = 0; i < masses.length; i++)
+       {
+           mass = masses[i];
+           fuelAnswer = fuelAnswers[i];
+
+           System.out.printf("Testing mass: %.0f\n", mass);
+
+           if(TestMasses(mass, fuelAnswer))
+           {
+               System.out.println("Test pass.");
+           }
+           else
+           {
+               System.out.println("Test fail!");
+               System.out.printf("\tCorrect answer is %.0f NOT %.0f\n", fuelAnswer, FuelCalculations(mass));
+           }
+
+           System.out.println();
+
+       }
+   }
+
+   public static boolean TestMasses(double mass, double fuelAnswer)
+   {
+       double fuel = FuelCalculations(mass);
+       if (fuel == fuelAnswer)
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+   }
+
+    private static double FuelCalculations(double mass) {
+        double fuel = 0;
+        double containerVolume = 3.0;
+        double fuelLoss = 2.0;
+
+        fuel = mass / containerVolume;
+        fuel = Math.round(fuel);
+        fuel -= fuelLoss;
+
+        return fuel;
+    }
+
+} 
