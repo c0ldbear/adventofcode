c375e0706fafc7d2a744c857e6a093c363344b2d
teddy <teddyjh@protonmail.com>
2019-12-01 12:37:54 +0100

    remove unused method and new test method
    We have removed a method that we do not need or use.
    
    Also, we are preparing for the next step in the assignment
    by creating a new test method (Tester02())

--- 2019/day01/main.java
+++ 2019/day01/main.java
@@ -5,10 +5,49 @@ class main {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
-        Tester();
+        Tester01();
         Assignment01();
-    }
 
+        Tester02();
+
+    }
+    
+    public static void Tester02() {
+       double[] masses = {12, 14, 1969, 100756};
+       double[] fuelAnswers = {};
+       double mass;
+       double fuelTest;
+       double fuelAnswer;
+       double totalFuel = 0.0;
+       
+       for(int i = 0; i < masses.length; i++)
+       {
+           mass = masses[i];
+           fuelAnswer = fuelAnswers[i];
+           fuelTest = FuelCalculations(mass);
+
+           System.out.printf("Testing mass: %.0f\n", mass);
+
+           if(TestMasses(mass, fuelAnswer))
+           {
+               System.out.println("Test pass.");
+               System.out.printf("Answer: %.0f\n", fuelTest);
+           }
+           else
+           {
+               System.out.println("Test fail!");
+               System.out.printf("\tCorrect answer is %.0f NOT %.0f\n", fuelAnswer, fuelTest);
+               break;
+           }
+
+           totalFuel += fuelTest;
+           System.out.println();
+
+       }
+
+       System.out.printf("Total fuel needed: %.0f\n", totalFuel);
+
+   }
     public static void Assignment01() {
 
         File inputFile = new File("input.txt");
@@ -36,7 +75,7 @@ class main {
         
    }
 
-   public static void Tester() {
+   public static void Tester01() {
        double[] masses = {12, 14, 1969, 100756};
        double[] fuelAnswers = {2, 2, 654, 33583};
        double mass;
@@ -96,18 +135,6 @@ class main {
         fuel -= fuelLoss;
 
         return fuel;
-    }
-
-    private static double FuelRequirements(double[] masses)
-    {
-        double totalFuel = 0.0;
-
-        for(double mass : masses)
-        {
-            totalFuel += FuelCalculations(mass);
-        }
-        
-        return totalFuel;
     }
 
 } 
